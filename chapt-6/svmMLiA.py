624264bdcb4d399f01f61d494c0ca30de8fe7739
liuhuan0672@126.com <liuhuan0672@126.com>
2018-01-19 17:19:24 +0800

    chapt-6
    SMO简单版本实现

---
+++ chapt-6/svmMLiA.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+"""
+SMO算法实现
+@file: svmMLiA.py
+@time: 2018/1/19 15:35
+"""
+
+import random
+
+# 加载数据
+from numpy import *
+
+
+def loadDataSet(filename):
+    dataMat = []
+    labelMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr = line.strip().split('\t')
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    return dataMat, labelMat
+
+
+# 随机获取两个alpha值的下标，组成alpha对
+def selectJrand(i, m):
+    j = i
+    while j == i:
+        j = int(random.uniform(0, m))
+    return j
+
+
+def clipAlpha(aj, H, L):
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
+
+
+# SMO算法简化版实现
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    b = 0
+    m, n = shape(dataMatrix)
+    alphas = mat(zeros((m, 1)))
+    iter = 0
+
+    while iter < maxIter:
+        alphaPairsChanged = 0
+        for i in range(m):
+            fXi = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i, :].T)) + b
+            Ei = fXi - float(labelMat[i])
+
+
+if __name__ == '__main__':
+    dataArr, labelArr = loadDataSet('testSet.txt')
+    print(labelArr)
