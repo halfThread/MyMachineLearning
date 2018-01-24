04ab67af7171f1198d7a22bedac1136695d75e92
liuhuan0672@126.com <liuhuan0672@126.com>
2018-01-24 17:22:26 +0800

    chapt-7
    adaBoost分类器

--- chapt-6/svmMLiA.py
+++ chapt-6/svmMLiA.py
@@ -41,7 +41,7 @@ def clipAlpha(aj, H, L):
 
 
 # SMO算法简化版实现
-def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+def smoSimple1(dataMatIn, classLabels, C, toler, maxIter):
     dataMatrix = mat(dataMatIn)
     labelMat = mat(classLabels).transpose()
     b = 0
@@ -49,7 +49,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
     alphas = mat(zeros((m, 1)))
     iterNum = 0
 
-    while iter < maxIter:
+    while iterNum < maxIter:
         alphaPairsChanged = 0
         for i in range(m):
             fXi = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i, :].T)) + b
@@ -61,6 +61,39 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                 Ej = fXj - float(labelMat[i])
                 alphaIold = alphas[i].copy()
                 alphaJold = alphas[j].copy()
+                if labelMat[i] != labelArr[j]:
+                    L = max(0, alphas[j] - alphas[i])
+                    H = min(C, C + alphas[j] - alphas[i])
+                else:
+                    L = max(0, alphas[j] + alphas[i] - C)
+                    H = min(C, alphas[j] + alphas[i])
+                if L == H:
+                    print("L == H")
+                    continue
+                eta = 2.0 * dataMatrix[i, :] * dataMatrix[j, :].T - \
+                      dataMatrix[i, :] * dataMatrix[i, :].T - \
+                      dataMatrix[j, :] * dataMatrix[j, :].T
+                if eta >= 0:
+                    print("eta >= 0")
+                    continue
+                alphas[j] -= labelMat[j] * (Ei - Ej) / eta
+                alphas[j] = clipAlpha(alphas[j], H, L)
+                if abs(alphas[j] - alphaJold) < 0.00001:
+                    print("j not moving enough")
+                    continue
+                alphas[i] += labelMat[j] * labelMat[i] * (alphaJold - alphas[j])
+                b1 = b - Ei - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, :] * dataMatrix[i, :].T - \
+                     labelMat[j] * (alphas[j] - alphaJold) * dataMatrix[i, :] * dataMatrix[j, :].T
+                b2 = b - Ej - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, :] * dataMatrix[j, :].T - \
+                     labelMat[j] * (alphas[j] - alphaJold) * dataMatrix[j, :] * dataMatrix[j, :].T
+                if 0 < alphas[i] and C > alphas[i]:
+                    b = b1
+                elif 0 < alphas[j] and C > alphas[j]:
+                    b = b2
+                else:
+                    b = (b1 + b2) / 2.0
+                alphaPairsChanged += 1
+                print("iterNum: %d i: %d, pairs changed %d " % (iterNum, i, alphaPairsChanged))
         if alphaPairsChanged == 0:
             iterNum += 1
         else:
@@ -69,6 +102,58 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
     return b, alphas
 
 
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    b = 0
+    m, n = shape(dataMatrix)
+    alphas = mat(zeros((m, 1)))
+    iterNum = 0
+    while iterNum < maxIter:
+        alphaPairsChanged = 0
+        for i in range(m):
+            fXi = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i, :].T)) + b
+            Ei = fXi - float(labelMat[i])  # if checks if an example violates KKT conditions
+            if ((labelMat[i] * Ei < -toler) and (alphas[i] < C)) or ((labelMat[i] * Ei > toler) and (alphas[i] > 0)):
+                j = selectJrand(i, m)
+                fXj = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[j, :].T)) + b
+                Ej = fXj - float(labelMat[j])
+                alphaIold = alphas[i].copy()
+                alphaJold = alphas[j].copy()
+                if labelMat[i] != labelMat[j]:
+                    L = max(0, alphas[j] - alphas[i])
+                    H = min(C, C + alphas[j] - alphas[i])
+                else:
+                    L = max(0, alphas[j] + alphas[i] - C)
+                    H = min(C, alphas[j] + alphas[i])
+                if L == H: print("L==H");continue
+            eta = 2.0 * dataMatrix[i, :] * dataMatrix[j, :].T - dataMatrix[i, :] * dataMatrix[i, :].T - \
+                  dataMatrix[j, :] * dataMatrix[j, :].T
+            if eta >= 0: print("eta>=0");continue
+        alphas[j] -= labelMat[j] * (Ei - Ej) / eta
+        alphas[j] = clipAlpha(alphas[j], H, L)
+        if abs(alphas[j] - alphaJold) < 0.00001: print("j not moving enough");continue
+        alphas[i] += labelMat[j] * labelMat[i] * (alphaJold - alphas[j])  # update i by the same amount as j
+        # the update is in the oppostie direction
+        b1 = b - Ei - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, :] * dataMatrix[i, :].T - \
+             labelMat[j] * (alphas[j] - alphaJold) * dataMatrix[i, :] * dataMatrix[j, :].T
+        b2 = b - Ej - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, :] * dataMatrix[j, :].T - \
+             labelMat[j] * (alphas[j] - alphaJold) * dataMatrix[j, :] * dataMatrix[j, :].T
+        if (0 < alphas[i]) and (C > alphas[i]):
+            b = b1
+        elif (0 < alphas[j]) and (C > alphas[j]):
+            b = b2
+        else:
+            b = (b1 + b2) / 2.0
+        alphaPairsChanged += 1
+        print("iterNum: %d i:%d, pairs changed %d" % (iterNum, i, alphaPairsChanged))
+    if alphaPairsChanged == 0:
+        iterNum += 1
+    else:
+        iterNum = 0
+    print("iteration number: %d" % iterNum)
+
+
 if __name__ == '__main__':
     dataArr, labelArr = loadDataSet('testSet.txt')
-    print(labelArr)
+    smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
---
+++ chapt-7/adaboost.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+"""
+@file: adaboost.py
+@time: 2018/1/24 14:06
+"""
+from numpy import *
+
+
+def loadSimpleData():
+    datMat = matrix([[1., 2.1], [2., 1.1], [1.3, 1.], [1., 1.], [2., 1.]])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+    return datMat, classLabels
+
+
+def stumpClassify(dataMatrix, dimen, threshVal, threshIneq):
+    retArray = ones((shape(dataMatrix)[0], 1))
+    if threshIneq == 'lt':
+        retArray[dataMatrix[:, dimen] <= threshVal] = -1.0
+    else:
+        retArray[dataMatrix[:, dimen] > threshVal] = -1.0
+    return retArray
+
+
+def buildStump(dataArr, classLabels, D):
+    dataMatrix = mat(dataArr)
+    labelMat = mat(classLabels).T
+    m, n = shape(dataMatrix)
+    numSteps = 10.0
+    bestStump = {}
+    bestClassEst = mat(zeros((m, 1)))
+    minError = inf  # 正无穷大
+    for i in range(n):  # 对每个特征进行遍历
+        rangeMin = dataMatrix[:, i].min()
+        rangeMax = dataMatrix[:, i].max()
+        stepSize = (rangeMax - rangeMin) / numSteps
+        for j in range(-1, int(numSteps) + 1):
+            for inequal in ['lt', 'gt']:
+                threshVal = (rangeMin + float(j) * stepSize)
+                predictedVals = stumpClassify(dataMatrix, i, threshVal, inequal)
+
+
+if __name__ == '__main__':
+    dataMat, classLabels = loadSimpleData()
+    D = mat(ones((5, 1)) / 5)
+    buildStump(dataMat, classLabels, D)
